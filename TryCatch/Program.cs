7853965719d51aa69f837e9ba1804752d74b8d66
Fikri Ulaş Osmanağaoğlu <2014010213010@ogrenci.karabuk.edu.tr>
2021-06-25 17:20:41 +0300

    Try Catch eklendi.

---
+++ TryCatch/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace TryCatch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Bir Sayı Giriniz: ");
+                int sayi = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Sayi: "+ sayi);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Hata: "+ex.Message);
+            }
+            finally
+            {
+                Console.WriteLine("Hata ile karşılaşıldı.");
+            }
+
+            // try
+            // {
+            //     int a = int.Parse(null);
+            // }
+            // catch (ArgumentException ex)
+            // {
+            //     Console.WriteLine("**Boş Değer**");
+            //     Console.WriteLine(ex);                
+            // }
+            try
+            {
+                int a = int.Parse("Test");
+            }
+            catch (FormatException ex)
+            {
+                Console.WriteLine("**veri tipi uygun değil**");
+                Console.WriteLine(ex);                
+            }
+            
+        }
+    }
+}
