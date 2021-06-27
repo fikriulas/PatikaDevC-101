ff168848a1135683fd9d5e0cd79367e35545408e
Fikri Ulaş Osmanağaoğlu <2014010213010@ogrenci.karabuk.edu.tr>
2021-06-27 11:28:44 +0300

    if-else eklendi.

---
+++ if-else/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace if_else
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int time = DateTime.Now.Hour;
+
+            if(time >=6 && time<11)
+            {
+                Console.WriteLine("Günaydın");
+            }
+            else if (time <= 18)
+            {
+                Console.WriteLine("İyi Günler");
+            }
+            else{
+                Console.WriteLine("İyi Akşamlar");
+            }
+
+            string sonuc = time <=18 ? "İyi günler" : "İyi akşamlar";
+            Console.WriteLine(sonuc);
+
+            sonuc = time >=6 && time <= 12 ? "Günaydın" : time <= 18 ? "İyi günler":"iyi geceler";
+            Console.WriteLine(sonuc);
+
+        }
+    }
+}
+ 
