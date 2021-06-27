3b1537005e00786fa645bb2daf0d0db929c78342
Fikri Ulaş Osmanağaoğlu <2014010213010@ogrenci.karabuk.edu.tr>
2021-06-27 12:11:13 +0300

    Forloop Eklendi

---
+++ forloop/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace forloop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Console.WriteLine("Bir sayı girin:");
+            // int sayac = int.Parse(Console.ReadLine());
+            // for (int i = 0; i < sayac; i++)
+            // {
+            //     if(i % 2 == 1)
+            //         Console.WriteLine(i);
+            // }
+            // //1 ile 100 arasındaki tek ve çift sayıların toplamı
+            // int tektoplam = 0;
+            // int ciftoplam = 0;
+            // for (int i = 1; i<= 100; i++)
+            // {
+            //     if (i % 2 == 1)
+            //     {
+            //         tektoplam += i;
+            //     }
+            //     else{
+            //         ciftoplam += i;
+            //     }
+            // }
+            // Console.WriteLine("Çift Toplam: "+ciftoplam);
+            // Console.WriteLine("Tek Toplam: "+tektoplam);
+
+            for (int i = 0; i < 10; i++)
+            {
+                if(i == 5)
+                    break;
+                Console.WriteLine(i);
+            }
+            for (int i = 0; i < 10; i++)
+            {
+                if(i == 5)
+                    continue;
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
