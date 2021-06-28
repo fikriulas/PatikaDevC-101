bfef2c689ad5996b3d5b6ee6efb6da77f6a1a539
Fikri Ulaş Osmanağaoğlu <2014010213010@ogrenci.karabuk.edu.tr>
2021-06-28 12:06:13 +0300

    While-forach eklendi.

---
+++ while-foreach/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace while_foreach
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Bir sayı giriniz: ");
+            int sayi = int.Parse(Console.ReadLine());
+            int toplam = 0;
+            int counter = 1;
+            while (counter <= sayi)
+            {
+                toplam += counter;
+                counter++;
+            }
+            Console.WriteLine("Ortalama: "+(toplam/sayi));
+
+            char character = 'a';
+            while (character <= 'z')
+            {
+                Console.Write(character);
+                character++;
+            }
+            string[] arabalar = {"BMW","Ford","Toyata","Nissan"};
+            foreach (var item in arabalar)
+            {
+                Console.WriteLine(item);
+            }
+
+        }
+    }
+}
